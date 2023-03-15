2c21f0b6fae5d803fe9a10971dc8557c7b4f4e05
akshay <akshaynr25@gmail.com>
2023-03-15 22:55:44 +0530

    modifi file

--- main.tf
+++ main.tf
@@ -30,6 +30,7 @@ resource "aws_subnet" "main1" {
     tags = {
       "Name" = "private_subnet1"
     }
+}
 resource "aws_subnet" "main2" {
     vpc_id = "vpc-04a281649bc33d9db"
     cidr_block = "10.0.0.128/26"
